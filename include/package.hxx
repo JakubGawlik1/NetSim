aea0ecd721e02e80b94d8d5776d207cb271bbecf
Karol Joniec <jonieckarol7@gmail.com>
2026-01-11 15:08:10 +0100

    inicjalizacja klasy Package

--- include/package.hxx
+++ include/package.hxx
@@ -1,7 +1,21 @@
 #pragma once
-
 #include "types.hxx"
 
 class Package {
 
+    public:
+    Package();
+    explicit Package(ElementID id);
+
+    Package(Package&&) noexcept;
+    Package& operator=(Package&&) noexcept;
+
+    Package(const Package&) = delete;
+    Package& operator=(const Package&) = delete;
+
+    ElementID get_id() const;
+    ~Package();
+
+    private:
+    ElementID id_;
 };
--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,10 @@
 #include "package.hxx"
+
+Package::Package(): id_(0) {}
+Package::Package(ElementID id): id_(id){}
+
+
+ElementID Package::get_id() const {
+    return id_;
+}
+
