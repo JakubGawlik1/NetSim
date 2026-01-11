ef9b3dc9a8f8e68dd5268a6e4b5527615fa234c6
Karol Joniec <jonieckarol7@gmail.com>
2026-01-11 16:00:37 +0100

    początek pracy packagequeu - wstepne uzupelnienie hpp

--- include/storage_types.hxx
+++ include/storage_types.hxx
@@ -1,10 +1,34 @@
 #pragma once
 
+#include <list>
+#include "package.hxx"
+
+//Package to polprodukt z ID
+
+//typ kolejki
 enum class PackageQueueType {
-    FIFO,
-    LIFO
+    FIFO, //pierwsza ktora przyszla (bierz z dolu)
+    LIFO //ostatnia ktora przyszla (bierz z gory)
 };
 
 class IPackageStockpile {
+    using const_iterator = std::list<Package>::const_iterator;
+
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator end() const = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator cend() const = 0;
+
+    virtual ~IPackageStockpile() = default;
+};
+
+class IPackageQueue : public IPackageStockpile {
 
 };
+
+
+//
+class PackageQueue: public IPackageQueue {
+
+};
+
