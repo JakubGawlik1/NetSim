6d0f3ef1d5785b59b89e4fcfa37d6e291327fefe
SzymonGrudzien <426248@student.agh.edu.pl>
2026-01-20 14:26:36 +0100

    Dodanie funkcji has_reachable_storehouse

--- include/factory.hxx
+++ include/factory.hxx
@@ -162,3 +162,5 @@ ParsedLineData parse_line(std::string& line);
 Factory load_factory_structure(std::istream& is);
 
 void save_factory_structure(Factory& factory, std::ostream& os);
+
+bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
